0776e585634066a8e1c1cadf97fd6b7965a90d58
LearnPRG-py <aryankrishnan4b@gmail.com>
2025-12-10 19:26:24 +0530

    Add nexJs

---
+++ _next/static/3_zLWmhVdf3kOekLGY3S9/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/dfe9f51e98cad549.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
